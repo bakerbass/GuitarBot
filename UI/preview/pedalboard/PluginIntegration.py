8c432ae7151cc8875aa5f8a897d37387eca8d6a0
Jack Keller <jackrobert0627@hotmail.com>
2023-12-11 17:58:15 -0500

    wrote plugin integration script

---
+++ UI/preview/pedalboard/PluginIntegration.py
@@ -0,0 +1,38 @@
+from mido import Message
+import time
+import rtmidi
+
+# NOTE: Designed to integrate with GarageBand. GarageBand must be running with an open software instrument track with the desired plugin loaded.
+PORT_NAME = 'GarageBand Virtual Out'
+midi_channel = 0
+
+def send_midi_to_plugin(midi_chords: list):
+    # open virtual midi port
+    midiout = rtmidi.MidiOut()
+    midiout.open_virtual_port(PORT_NAME)
+
+    with midiout:
+        time.sleep(1) # must have this for the first note_on message to send (TODO: maybe a better fix out there?)
+
+        # send messages to adjust settings
+        midiout.send_message(Message(type='note_on', note=85, channel=midi_channel, velocity=1).bytes()) # strum mode off
+        midiout.send_message(Message(type='note_on', note=92, channel=midi_channel, velocity=1).bytes()) # open string first mode off
+
+        # loop through midi messages and send to plugin
+        # midi_chords is of format: [(note_on messages, note_off messages, duration), chord2, chord3, etc...]
+        for chord in midi_chords:
+            note_ons = chord[0]
+            note_offs = chord[1]
+            duration = chord[2]
+
+            for msg in note_ons:
+                midiout.send_message(msg) # make sure this is in bytes
+
+            time.sleep(duration)
+            
+            for msg in note_offs:
+                midiout.send_message(msg) # make sure this is in bytes
+
+        # send note_off's for settings messages
+        midiout.send_message(Message(type='note_off', note=85, channel=midi_channel, velocity=1).bytes())
+        midiout.send_message(Message(type='note_off', note=92, channel=midi_channel, velocity=1).bytes())
--- UI/preview/pedalboard/midi_test_script.py
+++ UI/preview/pedalboard/midi_test_script.py
@@ -2,6 +2,8 @@ from mido import Message
 import time
 import rtmidi
 
+# NOTE: Designed to integrate with GarageBand.
+
 PORT_NAME = 'GarageBand Virtual Out'
 
 # open virtual midi port
@@ -11,13 +13,13 @@ midiout = rtmidi.MidiOut()
 midiout.open_virtual_port(PORT_NAME)
 
 with midiout:
-    channel = 1
+    channel = 0
     root = 48
-    maj_third_offset = 4
+    third_offset = 4 # 4 = major, 3 = minor
     fifth_offset = 7
     octave_offset = 12
 
-    time.sleep(0.5) # must have this for the note_on message to send (TODO: maybe a better fix out there?)
+    time.sleep(1) # must have this for the note_on message to send (TODO: maybe a better fix out there?)
 
     # send messages to adjust settings
     midiout.send_message(Message(type='note_on', note=85, channel=channel, velocity=1).bytes()) # strum mode off
@@ -26,20 +28,20 @@ with midiout:
     # play chord
     # TODO: delay between notes for more realistic strumming? May not be needed
     midiout.send_message(Message(type='note_on', note=root, channel=channel, velocity=64).bytes())
-    midiout.send_message(Message(type='note_on', note=root + maj_third_offset, channel=channel, velocity=64).bytes())
+    midiout.send_message(Message(type='note_on', note=root + third_offset, channel=channel, velocity=64).bytes())
     midiout.send_message(Message(type='note_on', note=root + fifth_offset, channel=channel, velocity=64).bytes())
     midiout.send_message(Message(type='note_on', note=root + octave_offset, channel=channel, velocity=64).bytes())
-    midiout.send_message(Message(type='note_on', note=root + octave_offset + maj_third_offset, channel=channel, velocity=64).bytes())
+    midiout.send_message(Message(type='note_on', note=root + octave_offset + third_offset, channel=channel, velocity=64).bytes())
 
     # pause for chord duration
     time.sleep(3)
 
     # stop chord
     midiout.send_message(Message(type='note_off',  note=root, channel=channel).bytes())
-    midiout.send_message(Message(type='note_off', note=root + maj_third_offset, channel=channel).bytes())
+    midiout.send_message(Message(type='note_off', note=root + third_offset, channel=channel).bytes())
     midiout.send_message(Message(type='note_off', note=root + fifth_offset, channel=channel).bytes())
     midiout.send_message(Message(type='note_off', note=root + octave_offset, channel=channel).bytes())
-    midiout.send_message(Message(type='note_off', note=root + octave_offset + maj_third_offset, channel=channel).bytes())
+    midiout.send_message(Message(type='note_off', note=root + octave_offset + third_offset, channel=channel).bytes())
 
     # send note_off's for settings messages
     midiout.send_message(Message(type='note_off', note=85, channel=channel, velocity=1).bytes())

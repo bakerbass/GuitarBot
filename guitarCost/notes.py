24d1039bde0947466de36c2d302d8fa84d63b628
Katherine Sanders <68821698+katherinesanders@users.noreply.github.com>
2024-04-09 11:21:40 -0400

    Update notes.py
    if-else blocks --> map for simplicity

--- guitarCost/notes.py
+++ guitarCost/notes.py
@@ -1,73 +1,18 @@
 def notePossibilities(desired_note):
-    if desired_note == "E":
-        return [0, -1, -1, -1, -1, -1]
-    elif desired_note == "F":
-        return [1, -1, -1, -1, -1, -1]
-    elif desired_note == "Gb":
-        return [2, -1, -1, -1, -1, -1]
-    elif desired_note == "G":
-        return [3, -1, -1, -1, -1, -1]
-    elif desired_note == "Ab":
-        return [4, -1, -1, -1, -1, -1]
-    elif desired_note == "A":
-        return [5, 0, -1, -1, -1, -1]
-    elif desired_note == "Bb":
-        return [6, 1, -1, -1, -1, -1]
-    elif desired_note == "B":
-        return [7, 2, -1, -1, -1, -1]
-    elif desired_note == "C":
-        return [8, 3, -1, -1, -1, -1]
-    elif desired_note == "Db":
-        return [9, 4, -1, -1, -1, -1]
-    elif desired_note == "D":
-        return [10, 5, 0, -1, -1, -1]
-    elif desired_note == "Eb":
-        return [-1, 6, 1, -1, -1, -1]
-    elif desired_note == "E2":
-        return [-1, 7, 2, -1, -1, -1]
-    elif desired_note == "F2":
-        return [-1, 8, 3, -1, -1, -1]
-    elif desired_note == "Gb2":
-        return [-1, 9, 4, -1, -1, -1]
-    elif desired_note == "G2":
-        return [-1, 10, 5, 0, -1, -1]
-    elif desired_note == "Ab2":
-        return [-1, -1, 6, 1, -1, -1]
-    elif desired_note == "A2":
-        return [-1, -1, 7, 2, -1, -1]
-    elif desired_note == "Bb2":
-        return [-1, -1, 8, 3, -1, -1]
-    elif desired_note == "B2":
-        return [-1, -1, 9, 4, 0, -1]
-    elif desired_note == "C2":
-        return [-1, -1, 10, 5, 1, -1]
-    elif desired_note == "Db2":
-        return [-1, -1, -1, 6, 2, -1]
-    elif desired_note == "D2":
-        return [-1, -1, -1, 7, 3, -1]
-    elif desired_note == "Eb2":
-        return [-1, -1, -1, 8, 4, -1]
-    elif desired_note == "E3":
-        return [-1, -1, -1, 9, 5, 0]
-    elif desired_note == "F3":
-        return [-1, -1, -1, 10, 6, 1]
-    elif desired_note == "Gb3":
-        return [-1, -1, -1, -1, 7, 2]
-    elif desired_note == "G3":
-        return [-1, -1, -1, -1, 8, 3]
-    elif desired_note == "Ab3":
-        return [-1, -1, -1, -1, 9, 4]
-    elif desired_note == "A3":
-        return [-1, -1, -1, -1, 10, 5]
-    elif desired_note == "Bb3":
-        return [-1, -1, -1, -1, -1, 6]
-    elif desired_note == "B3":
-        return [-1, -1, -1, -1, -1, 7]
-    elif desired_note == "C3":
-        return [-1, -1, -1, -1, -1, 8]
-    elif desired_note == "Db3":
-        return [-1, -1, -1, -1, -1, 9]
-    elif desired_note == "D3":
-        return [-1, -1, -1, -1, -1, 10]
-    else:
-        return [-1, -1, -1, -1, -1, -1]
+    note_map = {
+        "E": [0, -1, -1, -1, -1, -1], "F": [1, -1, -1, -1, -1, -1], "Gb": [2, -1, -1, -1, -1, -1],
+        "G": [3, -1, -1, -1, -1, -1], "Ab": [4, -1, -1, -1, -1, -1], "A": [5, 0, -1, -1, -1, -1],
+        "Bb": [6, 1, -1, -1, -1, -1], "B": [7, 2, -1, -1, -1, -1], "C": [8, 3, -1, -1, -1, -1],
+        "Db": [9, 4, -1, -1, -1, -1], "D": [10, 5, 0, -1, -1, -1], "Eb": [-1, 6, 1, -1, -1, -1],
+        "E2": [-1, 7, 2, -1, -1, -1], "F2": [-1, 8, 3, -1, -1, -1], "Gb2": [-1, 9, 4, -1, -1, -1],
+        "G2": [-1, 10, 5, 0, -1, -1], "Ab2": [-1, -1, 6, 1, -1, -1], "A2": [-1, -1, 7, 2, -1, -1],
+        "Bb2": [-1, -1, 8, 3, -1, -1], "B2": [-1, -1, 9, 4, 0, -1], "C2": [-1, -1, 10, 5, 1, -1],
+        "Db2": [-1, -1, -1, 6, 2, -1], "D2": [-1, -1, -1, 7, 3, -1], "Eb2": [-1, -1, -1, 8, 4, -1],
+        "E3": [-1, -1, -1, 9, 5, 0], "F3": [-1, -1, -1, 10, 6, 1], "Gb3": [-1, -1, -1, -1, 7, 2],
+        "G3": [-1, -1, -1, -1, 8, 3], "Ab3": [-1, -1, -1, -1, 9, 4], "A3": [-1, -1, -1, -1, 10, 5],
+        "Bb3": [-1, -1, -1, -1, -1, 6], "B3": [-1, -1, -1, -1, -1, 7], "C3": [-1, -1, -1, -1, -1, 8],
+        "Db3": [-1, -1, -1, -1, -1, 9], "D3": [-1, -1, -1, -1, -1, 10]
+    }
+
+    return note_map.get(desired_note, [-1, -1, -1, -1, -1, -1])
+

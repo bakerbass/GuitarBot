8af007ef178a91000ed41abf2078d0ce0dc607eb
Jack Keller <jackrobert0627@hotmail.com>
2024-04-15 15:10:49 -0400

    Make sending and receiving IP the same

--- UI/messaging/ArmListSender.py
+++ UI/messaging/ArmListSender.py
@@ -5,7 +5,7 @@ from messaging.udp_constants import *
 class ArmListSender:
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.udp_ip = LOCAL_UDP_IP
+        self.udp_ip = SENDER_RECEIVER_IP
         self.udp_port = UDP_PORT
         self.left_arm_header = LEFT_ARM_HEADER
         self.right_arm_header = RIGHT_ARM_HEADER
--- UI/messaging/udp_constants.py
+++ UI/messaging/udp_constants.py
@@ -1,7 +1,10 @@
 # IP/PORTS
-UDP_IP = ""
+GUITARBOT_UDP_IP = "192.168.1.50"
 LOCAL_UDP_IP = "127.0.0.1"
-UDP_PORT = 4444
+UDP_PORT = 4268 # (GBOT)
+
+# NOTE: change this depending on whether sending locally or from laptop to lab computer
+SENDER_RECEIVER_IP = LOCAL_UDP_IP
 
 # HEADER DEFINITIONS
 LEFT_ARM_HEADER = "l_arm_guitar"

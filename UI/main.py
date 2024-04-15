360718658647cc7e6b6fd47d3063dca50dd4b40d
Jack Keller <jackrobert0627@hotmail.com>
2024-04-15 15:37:07 -0400

    fix pathing

--- UI/main.py
+++ UI/main.py
@@ -1,6 +1,7 @@
 from Model import Model
 from View import View
 from Controller import Controller
+import os
 
 def main():
     view = View()
@@ -10,4 +11,8 @@ def main():
 
 # run this script to start the UI
 if __name__ == "__main__":
+    # set root directory as 'GuitarBot/UI'
+    root_dir = os.getcwd()
+    if root_dir[-2:] != "UI": 
+        os.chdir("UI")
     main()

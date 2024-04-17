f62ec55fae83c9533bfdc5771b9577dc8a0990cf
Jack Keller <jackrobert0627@hotmail.com>
2024-04-17 15:28:54 -0400

    update udp_definitions with messaging instructions

--- UI/messaging/udp_definitions.py
+++ UI/messaging/udp_definitions.py
@@ -1,11 +1,19 @@
+# INSTRUCTIONS:
+# IP address of computer used to send messages must be configured to "169.254.244.xxx" (first 9 digits must be the same as GuitarBot computer)
+# Subnet mask must also be set to "255.255.255.0"
+# Both of these should be set in network settings for the specific ethernet LAN network connecting the sender to the GuitarBot computer
+
 # IP/PORTS
-GUITARBOT_UDP_IP = "192.168.1.215"
+GUITARBOT_UDP_IP = "169.254.244.221"
 LOCAL_UDP_IP = "127.0.0.1"
 UDP_PORT = 4268 # (GBOT)
 
-# NOTE: change this depending on whether sending locally or from laptop to lab computer
-DESTINATION_IP = LOCAL_UDP_IP
-RECEIVER_IP = "0.0.0.0" # open to all sources. Should remain constant
+# SENDING/RECEIVING IPS
+# NOTE: change DESTINATION_IP depending on whether sending locally or from laptop to lab computer
+DESTINATION_IP = GUITARBOT_UDP_IP
+
+# Don't modify this
+RECEIVER_IP = "0.0.0.0" # open to all sources
 
 # HEADER DEFINITIONS
 LEFT_ARM_HEADER = "l_arm_guitar"

8711a4b079be2476586f12bed88ec18cad1f6b95
smirfend3 <smirfend3@gatech.edu>
2024-10-28 18:20:55 -0400

    working send_msg function

--- GuitarBot2023Files/LeftArm/Strikers/Strikers.ino
+++ GuitarBot2023Files/LeftArm/Strikers/Strikers.ino
@@ -15,6 +15,11 @@ IPAddress ip(10, 2, 1, 177); //ip address
 unsigned int localPort = 8888; //udp port to listen for packets on
 
 char packetBuffer[1024];
+uint8_t playcommand[6];
+uint8_t fret[6];
+uint8_t pickings[6];
+int8_t strumAngle;
+char event;
 
 EthernetUDP udp;
 
@@ -61,35 +66,32 @@ void setup() {
     delay(2000);
     
     LOG_LOG("Listening for commands...");   // "in format (ascii characters) <mode><id code><midi velocity>"
-    pController->executePluckTest(0);
+    //pController->executePluckTest(0);
 }
 
 void loop() {
-  int8_t commands[18];
+  //int8_t commands[18];
 //test all 6 sliders
-    ethernetEvent(commands);
+    //ethernetEvent(commands);
+    ethernetEvent();
     if (complete) {
-        uint8_t idCode;
-        uint8_t midiVelocity;
-        uint8_t chPressure;
-        char cMode;
-        uint8_t playcommand[6];
-        uint8_t fret[6];
-        uint8_t pickings[6];
 
-        Error_t err = parseCommand(commands, playcommand, fret, pickings);
+        // Error_t err = parseCommand(commands, playcommand, fret, pickings);
         complete = false;
         // //Unpress
 
-        if (err == kNoError) {
-          pController->executeSlide(fret[0], fret[1], fret[2], fret[3], fret[4], fret[5], playcommand[0], playcommand[1], playcommand[2], playcommand[3], playcommand[4], playcommand[5]);
-          LOG_LOG("playcommand 1: %i, playcommand 2: %i, playcommand 3: %i, playcommand 4: %i, playcommand 5: %i, playcommand 6: %i", playcommand[0], playcommand[1], playcommand[2], playcommand[3], playcommand[4], playcommand[5]);
-          LOG_LOG("fret 1: %i, fret 2: %i, fret 3: %i, fret 4: %i, fret 5: %i, fret 6: %i", fret[0], fret[1], fret[2], fret[3], fret[4], fret[5]);
-          LOG_LOG("pick 1: %i, pick 2: %i, pick 3: %i, pick 4: %i, pick 5: %i, pick 6: %i", pickings[0], pickings[1], pickings[2], pickings[3], pickings[4], pickings[5]);
-        }
-        else if (err == kCorruptedDataError) {
-          Serial.println("Corrupted data");
-        }
+        // if (err == kNoError) {
+        //   pController->executeSlide(fret[0], fret[1], fret[2], fret[3], fret[4], fret[5], playcommand[0], playcommand[1], playcommand[2], playcommand[3], playcommand[4], playcommand[5]);
+        //   LOG_LOG("playcommand 1: %i, playcommand 2: %i, playcommand 3: %i, playcommand 4: %i, playcommand 5: %i, playcommand 6: %i", playcommand[0], playcommand[1], playcommand[2], playcommand[3], playcommand[4], playcommand[5]);
+        //   LOG_LOG("fret 1: %i, fret 2: %i, fret 3: %i, fret 4: %i, fret 5: %i, fret 6: %i", fret[0], fret[1], fret[2], fret[3], fret[4], fret[5]);
+        //   LOG_LOG("pick 1: %i, pick 2: %i, pick 3: %i, pick 4: %i, pick 5: %i, pick 6: %i", pickings[0], pickings[1], pickings[2], pickings[3], pickings[4], pickings[5]);
+        // }
+        // else if (err == kCorruptedDataError) {
+        //   Serial.println("Corrupted data");
+        // }
+        LOG_LOG("press 1: %i, press 2: %i, press 3: %i, press 4: %i, press 5: %i, press 6: %i", playcommand[0], playcommand[1], playcommand[2], playcommand[3], playcommand[4], playcommand[5]);
+        LOG_LOG("slide 1: %i, slide 2: %i, slide 3: %i, slide 4: %i, slide 5: %i, slide 6: %i", fret[0], fret[1], fret[2], fret[3], fret[4], fret[5]);
+        LOG_LOG("strummer: %i", strumAngle);
         delay(10);
     }
     //sendEthernet();
@@ -107,6 +109,35 @@ void ethernetEvent(int8_t commands[]) {
         //Serial.print(commands[i]);
       }
       //Serial.println('\n');
+      complete = true;
+    }        
+}
+
+void ethernetEvent() {
+    int packetSize = udp.parsePacket();
+    if (packetSize) {
+      udp.read(packetBuffer, 1024);
+      //Convert each byte in packet_buffer to a uint8_t
+      event = packetBuffer[0];
+      if (event == 'L') {
+        LOG_LOG("LH event");
+        playcommand[0] = static_cast<uint8_t>(packetBuffer[1]);
+        playcommand[1] = static_cast<uint8_t>(packetBuffer[2]);
+        playcommand[2] = static_cast<uint8_t>(packetBuffer[3]);
+        playcommand[3] = static_cast<uint8_t>(packetBuffer[4]);
+        playcommand[4] = static_cast<uint8_t>(packetBuffer[5]);
+        playcommand[5] = static_cast<uint8_t>(packetBuffer[6]);
+        fret[0] = static_cast<uint8_t>(packetBuffer[7]);
+        fret[1] = static_cast<uint8_t>(packetBuffer[8]);
+        fret[2] = static_cast<uint8_t>(packetBuffer[9]);
+        fret[3] = static_cast<uint8_t>(packetBuffer[10]);
+        fret[4] = static_cast<uint8_t>(packetBuffer[11]);
+        fret[5] = static_cast<uint8_t>(packetBuffer[12]);
+      }
+      else if (event == 'S') {
+        LOG_LOG("Strum event");
+        strumAngle = packetBuffer[1];
+      }
       complete = true;
     }        
 }
--- send_msg_test.py
+++ send_msg_test.py
@@ -1,37 +1,71 @@
 import socket
 import struct
 import time
-from tkinter import Event
 
-import serial
-import numpy as np
+from matplotlib.cbook import flatten
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_ip = "10.2.1.177"
 udp_port = 8888
-router_left = bytes('/lguitar', 'utf8')
-router_picker = bytes('/rguitar', 'utf8')
+LH = bytes('L', 'utf8')
+strum = bytes('S', 'utf8')
 
-#Add/remove any LH/strum events as desired to test
-#Format of event is:
-# [event type (LH/strum/pick),
-# properties (LH can be slide/press lists, strum can be pick angle--45==down strum, -45==up strum, pick can be pick/don't pick),
-# time]
-LH = [['LH', [[1, 2, 2, 1, 1, 1], [1, 2, 2, 1, 1, 1]], 0], ['LH', [[1, 1, 1, 2, 3, 2], [3, 3, 1, 2, 2, 2]], 1.193478260869565], ['LH', [[1, 1, 5, 4, 3, 3], [3, 3, 2, 2, 2, 2]], 3.130434782608695]]
-strum = [['strum', 'U', 0.5217391304347826], ['strum', 'D', 1.565217391304348]]
-
-Events = []
-for event in LH:
-    Events.append(event)
-
-for event in strum:
-    Events.append(event)
-
-Events.sort(key=lambda x: x[2])
-
-start = time.time()
-for e in Events:
-    tNextEvent = e[2]
-    type = e[0]
+def send_msg(type, command):
+    message = None
+    flattened = []
     if type == 'LH':
-        chordtoplay = e[1]
+        message = LH
+        flattened = [i for list in command for i in list]
+    elif type == 'strum':
+        message = strum
+        flattened.append(command)
+    # arr = []
+    # index = 0
+    # for c in command:
+    #     for i in c:
+    #         print(i)
+    #         arr[index] = i
+    #         index += 1
+    print("command: ", flattened)
+    pCommand = struct.pack(f'<{len(flattened)}b', *flattened)
+    packed_data = message + pCommand
+    time.sleep(0.005)
+    # self.sock.sendto(bytes(msg, 'utf8'), (self.udp_ip, self.udp_port))
+    sock.sendto(packed_data, (udp_ip, udp_port))
+
+    time.sleep(0.01)
+    return 0
+
+def main():
+    #Add/remove any LH/strum events as desired to test
+    #Format of event is:
+    # [event type (LH/strum/pick),
+    # properties (LH can be slide+press lists, strum can be pick angle--45==down strum, -45==up strum, pick can be pick/don't pick),
+    # time]
+    LH = [['LH', [[1, 2, 2, 1, 1, 1], [1, 2, 2, 1, 1, 1]], 0], ['LH', [[1, 1, 1, 2, 3, 2], [3, 3, 1, 2, 2, 2]], 1.193478260869565], ['LH', [[1, 1, 5, 4, 3, 3], [3, 3, 2, 2, 2, 2]], 3.130434782608695]]
+    strum = [['strum', -45, 0.5217391304347826], ['strum', 45, 1.565217391304348]]
+
+    Events = []
+    for event in LH:
+        Events.append(event)
+
+    for event in strum:
+        Events.append(event)
+
+    Events.sort(key=lambda x: x[2])
+
+    start = time.time()
+    for e in Events:
+        tNextEvent = e[2]
+        eventType = e[0]
+        if eventType == 'LH':
+            send_msg(type= 'LH', command=e[1])
+        elif eventType == 'strum':
+            send_msg(type= 'strum', command=e[1])
+        tElapsed = time.time() - start
+        while tElapsed < tNextEvent:
+            time.sleep(0.0001)
+            tElapsed = time.time() - start
+
+if __name__ == "__main__":
+    main()

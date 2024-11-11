07f2452668050035460df61865b7492b757a8ce4
smirfend3 <smirfend3@gatech.edu>
2024-11-11 12:23:35 -0500

    Update epos4.cpp

--- GuitarBot2023Files/LeftArm/Strikers/src/epos4/epos4.cpp
+++ GuitarBot2023Files/LeftArm/Strikers/src/epos4/epos4.cpp
@@ -549,6 +549,9 @@ int Epos4::setOpMode(OpMode opMode, uint8_t uiInterpolationTime, int8_t iInterpo
         if(m_uiNodeID > 6 && m_uiNodeID < 13){
             n = SetHomeOffset(-25);
         }
+        if(m_uiNodeID == 7){
+            n = SetHomeOffset(0); //-25
+        }
 
         if(n != 0 ) {
             LOG_ERROR("setHomeOffset");
